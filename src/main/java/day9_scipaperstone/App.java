2c13a194ac376b2e41b0da2efb3b298aa9d39075
ridzy <ridzuanazmi.11@gmail.com>
2023-01-20 15:53:13 +0800

    Sciccor paper stone game incomplete

---
+++ src/main/java/day9_scipaperstone/App.java
@@ -0,0 +1,17 @@
+package day9_scipaperstone;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
